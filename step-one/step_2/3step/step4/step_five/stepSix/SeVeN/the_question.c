7002b7f425f3129c5ac70cb1fadb3766dd79cc69
SRStatz <srstatz@gmail.com>
2020-05-04 05:12:40 +0000

    chicken dinner

---
+++ step-one/step_2/3step/step4/step_five/stepSix/SeVeN/the_question.c
@@ -0,0 +1,5 @@
+240 gr (16 g) SJHP Remington	1,180 ft/s (360 m/s)	741 ft⋅lbf (1,005 J)
+240 gr (16 g) JHP Cor-Bon	1,475 ft/s (450 m/s)	1,160 ft⋅lbf (1,570 J)
+270 gr (17 g) LFN GC Buffalo bore heavy	1,450 ft/s (440 m/s)	1,260 ft⋅lbf (1,710 J)
+300 gr (19 g) JSP Cor-Bon	1,250 ft/s (380 m/s)	1,041 ft⋅lbf (1,411 J)
+340 gr (22 g) LFN +P+ Buffalo bore heavy	1,425 ft/s (434 m/s)	1,533 ft⋅lbf (2,078 J)S
